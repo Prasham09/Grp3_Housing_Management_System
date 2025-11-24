bfb047ea4049cf0982fe57274ec4b5aaac5dd212
tanya-bansal28 <bansal.t@northeastern.edu>
2025-11-23 23:47:27 -0500

    Insert data into ADMIN Table

---
+++ DataInsertionDML.sql
@@ -0,0 +1,13 @@
+show user;
+
+-- Inserting data into admin table while connecting as an HMS_ADMIN. ADMINS (10)
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm1pass','admin1@housing.edu','John Admin','617-555-0001');
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm2pass','admin2@housing.edu','Sarah Admin','617-555-0002');
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm3pass','admin3@housing.edu','Michael Admin','617-555-0003');
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm4pass','admin4@housing.edu','Emma Admin','617-555-0004');
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm5pass','admin5@housing.edu','David Admin','617-555-0005');
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm6pass','admin6@housing.edu','Olivia Admin','617-555-0006');
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm7pass','admin7@housing.edu','Liam Admin','617-555-0007');
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm8pass','admin8@housing.edu','Noah Admin','617-555-0008');
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm9pass','admin9@housing.edu','Ava Admin','617-555-0009');
+INSERT INTO ADMIN (password, email, name, phone) VALUES ('adm10pass','admin10@housing.edu','Sophia Admin','617-555-0010');
